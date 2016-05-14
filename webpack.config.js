e0fe96f6c622f2d7a909f13b653141cdf682a787
Simon Rothert <srothert@ziba-munich.com>
2016-05-14 09:46:08 +0200

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './app/app.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loaders: [ 'style', 'css', 'sass' ]
+            },
+            {
+               test: /\.html$/,
+               loader: 'raw-loader'
+            }
+        ]
+    },
+    sassLoader: {
+        includePaths: [path.resolve(__dirname, './app')]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['', '.js', '.scss', '.html'],
+        root: [path.resolve(__dirname, './app')]
+    },
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+          index: '/index.html'
+        }
+    }
+}
